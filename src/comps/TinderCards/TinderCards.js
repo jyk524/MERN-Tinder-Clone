a14b80c303a93a015ca9c3087afaa707de211b6f
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-11-11 08:57:45 -0500

    added tinderCards using react-tinder-card npm module

--- src/comps/TinderCards/TinderCards.js
+++ src/comps/TinderCards/TinderCards.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './TinderCards.css'
+import TinderCard from 'react-tinder-card'
 
 function TinderCards() {
 
@@ -16,13 +17,37 @@ function TinderCards() {
         },
     ])
 
+    const swiped = (direction, nameToDelete) => {
+        console.log("removing" + nameToDelete);
+        // setLastDirection(direction);
+    };
+
+    const outOfFrame = (name) => {
+        console.log(name + " left the screen!");
+    };
+
     return (
         <div className="tinderCards">
-            {people.map((person) => (
-                <h1>{person.name}</h1>
-            ))}
+            <div className="tinderCards__cardContainer">
+                {people.map((person) => (
+                <TinderCard
+                    className="swipe"
+                    key={person.name}
+                    preventSwipe={("up", "down")}
+                    onSwipe={(dir) => swiped(dir, person.name)}
+                    onCardLeftScreen={() => outOfFrame(person.name)}
+                >
+                    <div 
+                    style={{ backgroundImage: `url(${person.url})` }}
+                    className="card"
+                    >
+                        <h3>{person.name}</h3>
+                    </div>
+                </TinderCard>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
 
 export default TinderCards;
