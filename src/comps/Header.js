05745f3ca504fc17c4796bc99f0d23e660a61c98
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-11-11 08:03:28 -0500

    Header Buttons added

--- src/comps/Header.js
+++ src/comps/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Header.css';
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
     return (
         <div className='header'>
-            <h2>I am a Header</h2>
+            <IconButton>
+                <PersonIcon fontSize="large" className="header-icon" />
+            </IconButton>
+
+            <img 
+            className="header_logo"
+            src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+            alt=""
+            />
+            <IconButton>
+                <ForumIcon />
+            </IconButton>
         </div>
     )
 }
