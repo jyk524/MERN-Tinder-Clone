86f4da9260eb57e376b5994554b40ac73d6c5218
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-11-12 06:32:17 -0500

    Readme.md updated

--- src/comps/TinderCards/TinderCards.js
+++ src/comps/TinderCards/TinderCards.js
@@ -6,7 +6,7 @@ import axios from './axios'
 function TinderCards() {
 
     const[people, setPeople] = useState([]);
-
+    // This hook connects the front end to the backend using axios to set the URL to localhost 8000
     useEffect(() => {
         async function fetchData(){
             const req = await axios.get('/tinder/cards');
