8d975c0d8b163e8dc7388546d66e8884c0f31a98
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-11-12 06:23:05 -0500

    Hook for connecting backend to frontend, also changed person.url

--- src/comps/TinderCards/TinderCards.js
+++ src/comps/TinderCards/TinderCards.js
@@ -1,50 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './TinderCards.css'
 import TinderCard from 'react-tinder-card'
+import axios from './axios'
 
 function TinderCards() {
 
-    const[people, setPeople] = useState([
-        {
-            name: "Michael Scott",
-            url: 
-            "https://www.usmagazine.com/wp-content/uploads/2020/03/The-Office-Cast-Then-and-Now-Steve-Carell.jpg?w=700&quality=86&strip=all"
-        },
-        {
-            name: "Jim Halpert",
-            url: 
-            "https://www.usmagazine.com/wp-content/uploads/2020/03/The-Office-Cast-Then-and-Now-John-Krasinski.jpg?w=700&quality=86&strip=all"
-        },
-        {
-            name: "Dwight Schrute",
-            url: 
-            "https://www.usmagazine.com/wp-content/uploads/2020/03/The-Office-Cast-Then-and-Now-Rainn-Wilson.jpg?w=700&quality=86&strip=all"
-        },
-        {
-            name: "Pam Beesley",
-            url: 
-            "https://www.usmagazine.com/wp-content/uploads/2020/03/The-Office-Cast-Then-and-Now-Jenna-Fischer.jpg?w=700&quality=86&strip=all"
-        },
-        {
-            name: "Angela Martin",
-            url: 
-            "https://www.usmagazine.com/wp-content/uploads/2020/03/The-Office-Cast-Then-and-Now-Angela-Kinsey.jpg?w=700&quality=86&strip=all"
-        },
-        {
-            name: "Kelly Kapoor",
-            url: 
-            "https://www.usmagazine.com/wp-content/uploads/2020/03/The-Office-Cast-Then-and-Now-Mindy-Kaling.jpg?w=700&quality=86&strip=all"
-        },
-        {
-            name: "Meredith Palmer",
-            url: 
-            "https://www.usmagazine.com/wp-content/uploads/2020/03/The-Office-Cast-Then-and-Now-Kate-Flannery.jpg?w=700&quality=86&strip=all"
-        },
-        {
-            name: "Phyllis",
-            url: "https://www.usmagazine.com/wp-content/uploads/2020/03/The-Office-Cast-Then-and-Now-Phyllis-Smith.jpg?w=700&quality=86&strip=all"
+    const[people, setPeople] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const req = await axios.get('/tinder/cards');
+
+            setPeople(req.data);
         }
-    ])
+
+        fetchData();
+    }, [])
+
+    console.log(people)
 
     const swiped = (direction, nameToDelete) => {
         console.log("removing" + nameToDelete);
@@ -67,7 +40,7 @@ function TinderCards() {
                     onCardLeftScreen={() => outOfFrame(person.name)}
                 >
                     <div 
-                    style={{ backgroundImage: `url(${person.url})` }}
+                    style={{ backgroundImage: `url(${person.imgUrl})` }}
                     className="card"
                     >
                         <h3>{person.name}</h3>
